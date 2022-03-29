4c31f5943ace4a8806e69af2356e42408ccbc38b
SettleKim <101859908+SettleKim@users.noreply.github.com>
2022-03-29 21:21:30 +0900

    Update and rename 재민안/test.md to 김세준/Main.java

---
+++ 김세준/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+        int input1 = scanner.nextInt();
+        int input2 = scanner.nextInt();
+        int input3 = scanner.nextInt();
+        System.out.println("김세준\n202211271");
+        System.out.printf("%d@%d=%d\n",input1,input2,(input1+input2)*(input1-input2));
+        System.out.println((input1+input2)%input3==((input1%input3)+(input2%input3))%input3);
+        System.out.println((input1*input2)%input3==((input1%input3)*(input2%input3)%input3));
+    }
+}
